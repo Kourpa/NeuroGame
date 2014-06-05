cdba8e2a4c9cd31188c4f1fa09c43c074e91ef6c
Kourpa <mlemus01@unm.edu>
2014-06-04 22:48:13 -0600

    initializing git?

---
+++ src/neurogame/NeuroGame.java
@@ -0,0 +1,19 @@
+/**
+ * @author Marcos Lemus
+ * */
+package neurogame;
+
+/**
+ *
+ * @author Marcos
+ */
+public class NeuroGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+    }
+    
+}
